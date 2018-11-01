a109e19aac44f218dade4eb1be557b201841005a
thmsrbn <thomas.arbona@epitech.eu>
2018-11-01 22:59:49 +0100

    fix(sagas): fork

--- src/state/sagas/index.js
+++ src/state/sagas/index.js
@@ -1,7 +1,10 @@
+import { fork } from 'redux-saga/effects';
 import app from './app';
 
-const sagas = [
-  app
-];
+const sagas = function* () {
+  yield [
+	  fork(app),
+  ];
+}
 
 export default sagas;
